00d8619878331e12a6eefa0ecc481e067c2e4b9f
azurekite83 <azurekite83@gmail.com>
2020-02-15 20:52:12 -0600

    Finished skeleton of app, need to add products section. Interactivity will be added and lots of styling.

--- src/FuturaApp.tsx
+++ src/FuturaApp.tsx
@@ -1,7 +1,11 @@
-import 'bootstrap/dist/css/bootstrap.css';
-import React from 'react';
 import './FuturaApp.css';
+import React from 'react';
 import { Container, Row, Col, Nav } from 'react-bootstrap';
+
+import Img1 from './img/img1.jpg';
+import Img2 from './img/img2.jpg';
+import Img3 from './img/img3.jpg';
+import DownloadImg from './img/downloadImg.png'
 
 function FuturaApp() {
   return(
@@ -9,6 +13,8 @@ function FuturaApp() {
       <Header />
       <Welcome />
       <MissionStatement />
+      <CompanyPromotion />
+      <Downloads />
     </>
   );
 } 
@@ -44,8 +50,10 @@ function Welcome() {
         <h1>Welcome!</h1>
 
         <Col xs={6}>
-          <p>Futura Corporations is an early 2000 establishment made to lessen the carbon
-            footprint that we made by consuming too much fossil fuel.</p>
+          <p>
+            Futura Corporations is an early 2000 establishment made to lessen the carbon
+            footprint that we made by consuming too much fossil fuel.
+          </p>
         </Col>
       </Row>
     </Container>
@@ -59,7 +67,8 @@ function MissionStatement() {
         <Col xs={10}>
         <h3>Our Mission</h3>
 
-        <p> Our mission in Futura is to provide the most energy and cost efficient
+        <p> 
+          Our mission in Futura is to provide the most energy and cost efficient
           solution to cars, while also lessening the carbon footprint caused by overuse 
           of fossil fuels. By doing this we can provide a cleaner environment and help
           those that are affected by pollution in greater cities. We believe that saving
@@ -69,6 +78,115 @@ function MissionStatement() {
       </Row>
     </Container>
   );
+}
+
+const reviewDescriptions = ["Wow! Futura has really outdone itself! I've had this car since 2011 and the material has lasted even though the price of the car is cheap!",
+"At first I was skeptical, but it seems like Futura really has outdone itself. It's very efficient and the battery life is long lasting.",
+"I knew at first sight, just by looking at the car catalog that this buy was gonna be my best. Both energy efficient and lasting, and the aesthetics of the car are beautiful! Not to mention, customer service was great since this was one of my first cars, love it!"];
+
+const reviewElements = [{image: Img1, name: "Jim Doe", review: reviewDescriptions[0], id: 1}, 
+{image: Img2, name: "John Doe", review: reviewDescriptions[1], id: 2},
+{image: Img3, name: "Jacy Doe", review: reviewDescriptions[2], id: 3}];
+
+function ReviewList(props) {
+  const reviews = props.reviewElements;
+  const reviewsToHtml = reviews.map((element) => {
+      return (
+        <Row key={element.id}>
+          <Col xs={8} lg>
+            <h2>{element.name}</h2>
+            <img src={element.image} alt={element.name} className="img-fluid"/>
+
+            <p>{element.review}</p>
+          </Col>
+          
+        </Row>
+      );
+  });
+
+  return (
+    <Container>
+      {reviewsToHtml}
+    </Container>
+  );
+}
+function CompanyPromotion() {
+  return(
+    <>
+    <Container>
+      <Row>
+        <Col xs={12}>
+          <h3>Engineering</h3>
+
+          <p>
+            At Futura, our top engineers come up with 
+            well thought out solutions to make our products
+            more efficient. They devote their time and resources
+            to come up with better design to appeal the public and promote
+            a better aesthetic, even if the customer is not interested
+            in making a better environment. This helps to double sales and
+            aid in our mission to spread the use of electric cars.
+          </p>
+        </Col>
+      </Row>
+    </Container>
+    <Container>
+      <Row>
+        <Col xs={12}>
+          <h3>Sales</h3>
+
+          <p>
+            Because of our engineers and their devotion to design and efficiency,
+            our sales since 2000 have doubled and are increasing almost exponentially. 
+            <br/>
+            Here are some reviews on Futura electric cars:
+          </p>
+
+          <ReviewList reviewElements={reviewElements} />
+
+        </Col>
+      </Row>
+    </Container>
+    <Container>
+      <Row>
+        <Col xs={12}>
+          <h3>Delivery</h3>
+
+          <p>
+            At Futura, we deliver straight to you with our sophisticated
+            shipping app (which is included in the <a href="#downloads" id="download-link">download</a> section below)
+          </p>
+        </Col>
+      </Row>
+    </Container>
+    </>
+  );
+}
+
+function Downloads() {
+  return (
+    <Container>
+      <h3>Downloads</h3>
+
+      <Row>
+        <Col xs={4}>
+          <h4>Apple</h4>
+
+          <img src={DownloadImg} className="img-fluid" alt="download"/>
+        </Col>
+        <Col xs={4}>
+          <h4>Android</h4>
+
+          <img src={DownloadImg} className="img-fluid" alt="download"/>
+        </Col>
+        <Col xs={4}>
+          <h4>Desktop</h4>
+
+          <img src={DownloadImg} className="img-fluid" alt="download"/>
+        </Col>
+      </Row>
+    </Container>
+  )
 }
 
 export default FuturaApp;
